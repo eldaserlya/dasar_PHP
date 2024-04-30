1553145ae48566cf6154e6c2c1dbfdd4926090d8
eldaserlya <eldasrlyadwis@gmail.com>
2024-04-30 17:59:37 +0700

    latihan5

---
+++ latihan5.php
@@ -0,0 +1,20 @@
+<?php
+
+$warna = "hijau";
+switch ($warna) {
+    case "merah":
+        echo "warna adalah merah";
+        break;
+    case "kuning":
+        echo "warna adalah kuning";
+        break;
+    case "hijau":
+        echo "warna adalah hijau";
+        break;
+    default:
+        echo "warna tidak dikenali!";
+}
+echo "<br>";
+echo '<body style="background-color: palegreen;">';
+
+?>
