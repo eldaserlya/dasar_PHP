3eecb1ae71ba16aeb2e14155f4f4ee63b42e5fa9
eldaserlya <eldasrlyadwis@gmail.com>
2024-05-01 16:39:31 +0700

    latihan2

--- latihan2.php
+++ latihan2.php
@@ -6,11 +6,11 @@ $x = 5;
 $y = 10.5;
 
 echo '<body style="background-color: lightyellow;">';
-echo "<p>Isi Vareabel txt adalah: $txt</p>";
-echo "<p>Isi Vareabel x adalah: $x</p>";
-echo "<p>Isi Vareabel txt adalah: $y</p>";
-echo "Belajar PHP di" . $txt2 . "<br>";
-echo $x + $y;
+echo "<p>Isi Variabel txt adalah: $txt</p>";
+echo "<p>Isi Variabel x adalah: $x</p>";
+echo "<p>Isi Variabel y adalah: $y</p>";
+echo "Belajar PHP di "  . $txt2 . "<br>";
+echo $x + $y. "<br>";
 
 // PHP konstanta
 define("nama_konstanta", "{Elda Serlya}");
